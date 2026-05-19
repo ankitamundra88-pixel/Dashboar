a29bab672c0b028e94cc4a91c8583f8ad6cfd4e8
ankitamundra88-pixel <ankitamundra88@gmail.com>
2026-05-19 18:24:24 +1000

    Add files via upload

---
+++ Routine.py
@@ -0,0 +1,25 @@
+print("My Weekly Daily Routine")
+
+# step 1: Waking up
+print("I wake up at 6 am")
+
+# step 2: Going to school
+print("I go to school at 8:30am")
+
+# step 3: Playing after school
+print("I play with my friends in the evening")
+
+# step 4: Studying
+print("I study for 1 hour at night")
+
+# step 5: Watching TV
+print("I watch TV for 2 hours at night")
+
+# step 6: Sleeping
+print("I sleep at 10pm and sleep for 8 hours")
+
+# Calculating total study time in a week
+print("Total study hours in a week:", 2 * 7)
+
+# Calculating total watching time in a week
+print("Total Watching hours in a week:", 1 * 7)
